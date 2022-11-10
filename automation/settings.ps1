70dbb54a93b638459ff438092adb1a20256b3217
APACHƐ™ <anthonkrisat@hotmail.com>
2022-11-10 23:03:20 +0100

    make sure mods are up to date

--- automation/settings.ps1
+++ automation/settings.ps1
@@ -47,7 +47,7 @@ $CLIENT_CHANGELOG_TYPE = "markdown"
 
 # Changelog
 # Must be a single string.
-$CLIENT_CHANGELOG = "[![](https://i.imgur.com/o5aNlUD.jpg)](https://github.com/TeamAOF/All-of-Fabric-6/blob/main/changelogs/CHANGELOG.md)"
+$CLIENT_CHANGELOG = "[![](https://i.imgur.com/fT0iEaP.png)](https://github.com/TeamAOF/All-of-Fabric-6/blob/main/changelogs/CHANGELOG.md)"
 
 # =====================================================================//
 #  CURSEFORGE PROJECT SETTINGS
