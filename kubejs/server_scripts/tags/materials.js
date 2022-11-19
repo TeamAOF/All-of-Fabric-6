f5f6246a0ba74aef5f112485a63a14e82d891902
APACHƐ™ <anthonkrisat@hotmail.com>
2022-11-19 17:29:37 +0100

    add create to correct common tags

--- kubejs/server_scripts/tags/materials.js
+++ kubejs/server_scripts/tags/materials.js
@@ -5,6 +5,7 @@
 
 ServerEvents.tags('item', event => {
 
+    // Create (Common Tag Conversion)
     const create_materials = [
         'zinc'
     ];
@@ -15,5 +16,28 @@ ServerEvents.tags('item', event => {
         event.add("c:" + item +"_blocks", "create:" + item + "_block")
         event.add("c:" + item +"_dusts", "create:crushed_" + item + "_ore")
     });
+
+    const create_plates = [
+        'copper',
+        'iron',
+        'brass',
+    ];
+
+    create_plates.forEach((item) => {
+        event.add("c:" + item +"_plates", "create:" + item + "_sheet")
+    });
+
+    const create_crushed = [
+        'copper',
+        'iron',
+        'gold',
+    ];
+
+    create_crushed.forEach((item) => {
+        event.add("c:" + item +"_dusts", "create:crushed_" + item + "_ore")
+    });
+
+    event.add("c:gold_plates", "create:golden_sheet")
+    event.add("c:brass_ingots", "create:brass_ingot")
 
 });
