b97cdc0359764cd4c0dbe72a56ff57985f4015d9
APACHƐ™ <anthonkrisat@hotmail.com>
2022-11-19 19:36:13 +0100

    add crushed zinc ore recipe in mi macerator

--- kubejs/server_scripts/recipetypes/modern_industrialization/macerator.js
+++ kubejs/server_scripts/recipetypes/modern_industrialization/macerator.js
@@ -4,23 +4,42 @@
 
 ServerEvents.recipes(event => {
 
-  
-    event.custom({    
-        "type": "modern_industrialization:macerator",
-        "id": "aof:mi_macerator_pervaded_netherrack",
-        "eu": 2,
-        "duration": 100,
-        "item_inputs": [
-          {
-            "item": "byg:pervaded_netherrack",
-            "amount": 1
-          }
-        ],
-        "item_outputs": [
-          {
-            "item": "minecraft:glowstone_dust",
-            "amount": 2
-          }
-        ]
+
+  event.custom({
+    "type": "modern_industrialization:macerator",
+    "id": "aof:mi_macerator_pervaded_netherrack",
+    "eu": 2,
+    "duration": 200,
+    "item_inputs": [
+      {
+        "item": "byg:pervaded_netherrack",
+        "amount": 1
+      }
+    ],
+    "item_outputs": [
+      {
+        "item": "minecraft:glowstone_dust",
+        "amount": 2
+      }
+    ]
+  })
+
+  event.custom({
+    "type": "modern_industrialization:macerator",
+    "id": "aof:mi_macerator_crushed_zinc_ore",
+    "eu": 2,
+    "duration": 200,
+    "item_inputs": [
+      {
+        "tag": "c:ores/zinc",
+        "amount": 1
+      }
+    ],
+    "item_outputs": [
+      {
+        "item": "create:crushed_zinc_ore",
+        "amount": 2
+      }
+    ]
+  })
 })
-    })
