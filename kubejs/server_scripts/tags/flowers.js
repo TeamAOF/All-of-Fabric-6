3f79118385bf9626cb72190a81d8d1730b71d3a1
APACHƐ™ <anthonkrisat@hotmail.com>
2022-11-20 09:15:15 +0100

    fix incorrect flower tags

---
+++ kubejs/server_scripts/tags/flowers.js
@@ -0,0 +1,31 @@
+////////////////////////
+/// Made by Team AOF ///
+////////////////////////
+
+
+ServerEvents.tags('item', event => {
+
+    // Flowers conversion to allow bee pollination/locate
+    const small_flowers = [
+        'srias_flowers:blue_poppy',
+        'srias_flowers:hydrangea',
+        'srias_flowers:crocus',
+        'srias_flowers:snowdrops',
+        'srias_flowers:orange_daisy',
+        'ecologics:azalea_flower'
+    ];
+
+    const tall_flowers = [
+        'biomemakeover:foxglove',
+        'biomemakeover:black_thistle'
+    ];
+
+    small_flowers.forEach((item) => {
+        event.add("minecraft:small_flowers", item)
+    });
+
+    tall_flowers.forEach((item) => {
+        event.add("minecraft:tall_flowers", item)
+    });
+
+});
