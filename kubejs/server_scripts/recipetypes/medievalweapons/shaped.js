8862cd964e5f43e6d45883f4a10b30a85cd8b482
APACHƐ™ <anthonkrisat@hotmail.com>
2022-11-22 17:45:15 +0100

    more fixes for conflict between javelins & wands

--- kubejs/server_scripts/recipetypes/medievalweapons/shaped.js
+++ kubejs/server_scripts/recipetypes/medievalweapons/shaped.js
@@ -13,6 +13,24 @@ ServerEvents.recipes(event => {
         B: 'minecraft:stick'
     },
       id: 'medievalweapons:diamond_javelin'
+    },
+    {
+      output: 'medievalweapons:iron_javelin',
+      pattern: ['  A', ' B ', 'B  '],
+      key: {
+        A: 'farmersdelight:iron_knife',
+        B: 'minecraft:stick'
+    },
+      id: 'medievalweapons:iron_javelin'
+    },
+    {
+      output: 'medievalweapons:stone_javelin',
+      pattern: ['  A', ' B ', 'B  '],
+      key: {
+        A: 'minecraft:stone',
+        B: 'minecraft:stick'
+    },
+      id: 'medievalweapons:stone_javelin'
     }
   ];
 
