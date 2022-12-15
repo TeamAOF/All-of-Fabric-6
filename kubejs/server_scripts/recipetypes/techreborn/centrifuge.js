e291b54d2960da7b75a464e07b6a9f4512f5b312
AK9 <anthonkrisat@hotmail.com>
2022-12-15 21:39:22 +0100

    add carbon dust in tr centrifuge

---
+++ kubejs/server_scripts/recipetypes/techreborn/centrifuge.js
@@ -0,0 +1,24 @@
+////////////////////////
+/// Made by Team AOF ///
+////////////////////////
+
+
+ServerEvents.recipes(event => {
+
+  
+    event.custom({    
+      "type": "techreborn:centrifuge",
+      "power": 16,
+      "time": 600,
+      "ingredients": [
+        {
+          "tag": "c:coal_dusts"
+        }
+      ],
+      "results": [
+        {
+          "item": "modern_industrialization:carbon_dust"
+        }
+      ]
+})
+    })
