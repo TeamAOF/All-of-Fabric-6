c392e780dacc369e20ae729c54349b9863f96abe
AK9 <anthonkrisat@hotmail.com>
2022-12-30 00:30:53 +0100

    add flower block tags to sria's flowers

--- kubejs/server_scripts/tags/flowers.js
+++ kubejs/server_scripts/tags/flowers.js
@@ -7,17 +7,16 @@ ServerEvents.tags('item', event => {
 
     // Flowers conversion to allow bee pollination/locate
     const small_flowers = [
-        'srias_flowers:blue_poppy',
-        'srias_flowers:hydrangea',
-        'srias_flowers:crocus',
+        'srias_flowers:columbine',
         'srias_flowers:snowdrops',
-        'srias_flowers:orange_daisy',
+        'srias_flowers:aloe_vera',
         'ecologics:azalea_flower'
     ];
 
     const tall_flowers = [
         'biomemakeover:foxglove',
-        'biomemakeover:black_thistle'
+        'biomemakeover:black_thistle',
+        'srias_flowers:tall_hydrangea'
     ];
 
     small_flowers.forEach((item) => {
@@ -31,4 +30,32 @@ ServerEvents.tags('item', event => {
     // Dyes
     event.add("byg:brown_dye", "minecraft:cocoa_beans")
 
+});
+
+ServerEvents.tags('block', event => {
+
+    // Flowers conversion to allow bee pollination/locate
+    const small_flowers = [
+        'srias_flowers:columbine',
+        'srias_flowers:snowdrops',
+        'srias_flowers:aloe_vera',
+        'srias_flowers:fleabane',
+        'srias_flowers:himalayan_poppy',
+        'srias_flowers:hydrangea_bush',
+        'srias_flowers:sunrise_daisy'
+    ];
+
+    const tall_flowers = [
+        'srias_flowers:tall_hydrangea'
+    ];
+
+    small_flowers.forEach((item) => {
+        event.add("minecraft:small_flowers", item)
+        event.add("minecraft:flowers", item)
+    });
+
+    tall_flowers.forEach((item) => {
+        event.add("minecraft:tall_flowers", item)
+        event.add("minecraft:flowers", item)
+    });
 });
