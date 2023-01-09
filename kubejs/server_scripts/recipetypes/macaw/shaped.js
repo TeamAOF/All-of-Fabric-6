e91a9cc79d263e98db2c21b05c0c607994327e4d
AK9 <anthonkrisat@hotmail.com>
2023-01-09 20:21:51 +0100

    fix conflict between top roof and railings

--- kubejs/server_scripts/recipetypes/macaw/shaped.js
+++ kubejs/server_scripts/recipetypes/macaw/shaped.js
@@ -6,6 +6,7 @@
 
 ServerEvents.recipes(event => {
 
+  // Lamps
   const macaws_lamps = [
     'white',
     'orange',
@@ -23,28 +24,51 @@ ServerEvents.recipes(event => {
     'green',
     'black',
     'red'
-    ];
+  ];
 
-    macaws_lamps.forEach((item) => {
+  macaws_lamps.forEach((item) => {
     event.remove({ output: 'mcwlights:' + item + '_lamp' });
     event.remove({ output: 'mcwlights:' + item + '_ceiling_light' });
     event.shaped('6x mcwlights:' + item + '_lamp', [
-      'AAA', 
-      'ABA', 
+      'AAA',
+      'ABA',
       ' C '
     ], {
-        A: 'minecraft:' + item + '_wool',
-        B: 'minecraft:redstone_lamp',
-        C: '#c:fences/wooden'
-      })
-      event.shaped('6x mcwlights:' + item + '_ceiling_light', [
-        ' C ', 
-        'ABA', 
-        'AAA'
-      ], {
-          A: 'minecraft:' + item + '_wool',
-          B: 'minecraft:redstone_lamp',
-          C: '#c:fences/wooden'
-        })
-    });
+      A: 'minecraft:' + item + '_wool',
+      B: 'minecraft:redstone_lamp',
+      C: '#c:fences/wooden'
+    })
+    event.shaped('6x mcwlights:' + item + '_ceiling_light', [
+      ' C ',
+      'ABA',
+      'AAA'
+    ], {
+      A: 'minecraft:' + item + '_wool',
+      B: 'minecraft:redstone_lamp',
+      C: '#c:fences/wooden'
+    })
   });
+
+  // Top Roofs
+  const roofs = [
+    'oak',
+    'birch',
+    'spruce',
+    'jungle',
+    'dark_oak',
+    'acacia',
+    'crimson',
+    'warped',
+  ];
+
+  roofs.forEach((item) => {
+    event.remove({ output: 'mcwroofs:' + item + '_planks_top_roof' });
+    event.shaped('9x mcwroofs:' + item + '_planks_top_roof', [
+      ' A ',
+      'ABA',
+    ], {
+      A: 'minecraft:' + item + '_planks',
+      B: 'minecraft:stick',
+    })
+  });
+});
