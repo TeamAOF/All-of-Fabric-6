3a17080968835045022ec448eceba02c25bfc13c
AK9 <anthonkrisat@hotmail.com>
2023-01-27 16:40:44 +0100

    make cooler recipe harder

--- kubejs/server_scripts/recipetypes/kibe/shaped.js
+++ kubejs/server_scripts/recipetypes/kibe/shaped.js
@@ -13,4 +13,22 @@ ServerEvents.recipes(event => {
     ['modern_industrialization:diamond_plate',    '#minecraft:logs',                'modern_industrialization:diamond_plate'],
     ['modern_industrialization:gold_large_plate', '#minecraft:logs',                'modern_industrialization:gold_large_plate'],
   ]);
+
+  const recipes = [
+    {
+      output: 'kibe:cooler',
+      pattern: ['ADA', 'BCB', 'ABA'],
+      key: {
+        A: 'powah:dry_ice',
+        B: 'ad_astra:ice_shard',
+        C: '#c:chests',
+        D: 'minecraft:white_stained_glass'
+    },
+      id: 'kibe:cooler'
+    }
+  ];
+
+  recipes.forEach((recipe) => {
+    event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
+  });
 });
