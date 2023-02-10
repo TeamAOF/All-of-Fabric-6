3a1f41964dc5702584135f6dff9b42b1c089a3e3
Ellemes <3llemes@gmail.com>
2023-02-10 09:04:47 +0000

    Make chiselled bits a collapsible entry in REI.

---
+++ kubejs/client_scripts/collapsed_entries.js
@@ -0,0 +1,4 @@
+REIEvents.groupEntries(event => {
+    const block_bit = Item.of('chiselsandbits:block_bit')
+    event.groupSameItem('kubejs:rei_groups/chiselsandbits/block_bit', 'Chiselled bits', block_bit)
+});
