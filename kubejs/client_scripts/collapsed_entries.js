702d7ea9d84b6178e480718e37265c0d19743741
AK9 <anthonkrisat@hotmail.com>
2023-03-13 19:05:10 +0100

    remove b&c collapse script

--- kubejs/client_scripts/collapsed_entries.js
+++ kubejs/client_scripts/collapsed_entries.js
@@ -1,8 +1,3 @@
-REIEvents.groupEntries(event => {
-    const block_bit = Item.of('chiselsandbits:block_bit')
-    event.groupSameItem('kubejs:rei_groups/chiselsandbits/block_bit', 'Chiselled bits', block_bit)
-});
-
 function replaceAll(string, find, replace) {
     return string.replace(new RegExp(find, 'g'), replace)
 }
