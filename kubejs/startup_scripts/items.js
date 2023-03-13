241259d2be9c3e12abe64e822dc24d6dd2cb068c
AK9 <anthonkrisat@hotmail.com>
2023-03-13 04:56:08 +0100

    add All The Quests item

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -2,4 +2,5 @@ StartupEvents.registry('item', event => {
 
 	event.create('aof6_logo').displayName('AOF6')
 	event.create('completionist').displayName('Completionist Medal')
+	event.create('quester').displayName('All The Quests')
 });
