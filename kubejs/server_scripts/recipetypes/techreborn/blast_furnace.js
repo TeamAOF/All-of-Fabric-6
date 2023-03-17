0e5f49ae3d8a6e6a6ce5298fd8c5e00b925a93f3
AK9 <anthonkrisat@hotmail.com>
2023-03-17 07:41:15 +0100

    add titanium hot ingot in tr blast furnace

---
+++ kubejs/server_scripts/recipetypes/techreborn/blast_furnace.js
@@ -0,0 +1,24 @@
+////////////////////////
+/// Made by Team AOF ///
+////////////////////////
+
+
+ServerEvents.recipes(event => {
+
+event.custom({    
+  "type": "techreborn:blast_furnace",
+  "power": 128,
+  "time": 3600,
+  "heat": 1700,
+  "ingredients": [
+    {
+      "tag": "c:titanium_dusts"
+    }
+  ],
+  "results": [
+    {
+      "item": "modern_industrialization:titanium_hot_ingot"
+    }
+  ]
+})
+    })
