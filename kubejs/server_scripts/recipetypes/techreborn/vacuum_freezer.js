f5709977fb18ea4a3847163a179360507edd46fc
AK9 <anthonkrisat@hotmail.com>
2023-03-17 07:41:31 +0100

    add titanium ingot in tr vaccum freezer

--- kubejs/server_scripts/recipetypes/techreborn/vacuum_freezer.js
+++ kubejs/server_scripts/recipetypes/techreborn/vacuum_freezer.js
@@ -21,4 +21,19 @@ ServerEvents.recipes(event => {
         }
       ]
 })
+event.custom({    
+  "type": "techreborn:vacuum_freezer",
+  "power": 60,
+  "time": 400,
+  "ingredients": [
+    {
+      "item": "modern_industrialization:titanium_hot_ingot"
+    }
+  ],
+  "results": [
+    {
+      "item": "modern_industrialization:titanium_ingot"
+    }
+  ]
+})
     })
