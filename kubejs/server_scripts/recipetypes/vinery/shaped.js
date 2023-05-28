1f3ec5bf5d6a83550f769c7ce47835c18a610ccb
AK9 <anthonkrisat@hotmail.com>
2023-05-28 22:28:32 +0200

    fix table/chair recipes (beachparty/vinery)

--- kubejs/server_scripts/recipetypes/vinery/shaped.js
+++ kubejs/server_scripts/recipetypes/vinery/shaped.js
@@ -7,6 +7,8 @@
 ServerEvents.recipes(event => {
 
   const recipes = [
+
+    // Grapevine Steam
     {
       output: '6x vinery:grapevine_stem',
       pattern: ['A', 'A', 'A'],
@@ -14,7 +16,31 @@ ServerEvents.recipes(event => {
         A: '#minecraft:oak_logs',
       },
       id: 'vinery:grapevine_stem'
-    }
+    },
+
+    // Table
+    {
+      output: 'vinery:table',
+      pattern: ['BBB', 'A A', 'C C'],
+      key: {
+        A: 'minecraft:spruce_planks',
+        B: 'minecraft:spruce_slab',
+        C: 'minecraft:stick',
+      },
+      id: 'vinery:table'
+    },
+
+    // Chair
+    {
+      output: 'vinery:chair',
+      pattern: ['BAB', 'BBB', 'C C'],
+      key: {
+        A: 'minecraft:spruce_planks',
+        B: 'minecraft:spruce_slab',
+        C: 'minecraft:stick',
+      },
+      id: 'vinery:chair'
+    },
   ];
 
   recipes.forEach((recipe) => {
