5ad126a342e1c8105b9359e6e4b5b80f9f5f475c
Kazuhiko-Gushiken <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2023-07-04 19:36:58 -0700

    Delete items.js

--- kubejs/startup_scripts/items.js
+++
@@ -1,6 +0,0 @@
-StartupEvents.registry('item', event => {
-
-	event.create('aof6_logo').displayName('AOF6')
-	event.create('completionist').displayName('Completionist Medal')
-	event.create('quester').displayName('All The Quests')
-});
