76dba1cb6bd4d0dd6fa9d974d40bd9a047b3746c
Kazuhiko-Gushiken <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2023-07-04 19:37:04 -0700

    Add files via upload

---
+++ kubejs/startup_scripts/items.js
@@ -0,0 +1,11 @@
+StartupEvents.registry('item', event => {
+
+	event.create('aof6_logo').displayName('AOF6')
+	event.create('completionist').displayName('Completionist Medal')
+	event.create('quester').displayName('All The Quests')
+	event.create('croptopia_fertilizer').displayName('Croptopia Fertilizer')
+	event.create('empty_fertilizer_bag').displayName('Empty Fertilizer Bag')
+	event.create('glue').displayName('Glue')
+	event.create('croptopia_mulch').displayName('Croptopia Mulch')
+	event.create('el_risitas').displayName('El Risitas')
+});
