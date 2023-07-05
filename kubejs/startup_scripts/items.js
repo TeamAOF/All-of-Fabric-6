1789161c0c5df0d09b1558ffddf348f6c783a401
Kazuhiko-Gushiken <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2023-07-05 15:59:57 -0700

    Update items.js
    Added Invisible Ingots and Gear

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -7,4 +7,21 @@ StartupEvents.registry('item', event => {
 	event.create('empty_fertilizer_bag').displayName('Empty Fertilizer Bag') //original bag texture from sukuro120 on planetminecraft
 	event.create('glue').displayName('Glue')
 	event.create('croptopia_mulch').displayName('Croptopia Mulch') //original bag texture from sukuro120 on planetminecraft
+	event.create('invisible_ingot').displayName('Invisible Ingot').rarity('Common')
+	event.create('invisible_helmet', 'helmet').displayName('Invisible Helmet').tier('invisible').tooltip('§6Works in cosmetic slots too!')
+	event.create('invisible_chestplate', 'chestplate').displayName('Invisible Chestplate').tier('invisible').tooltip('§6Works in cosmetic slots too!')
+	event.create('invisible_leggings', 'leggings').displayName('Invisible Leggings').tier('invisible').tooltip('§6Works in cosmetic slots too!')
+	event.create('invisible_boots', 'boots').displayName('Invisible Boots').tier('invisible').tooltip('§6Works in cosmetic slots too!')
+});
+
+ItemEvents.armorTierRegistry(event => {
+    event.add('invisible', tier => {
+        tier.durabilityMultiplier = 5;
+        tier.slotProtections = [1, 1, 1, 1];
+        tier.enchantmentValue = 5;
+        tier.equipSound = 'minecraft:item.armor.equip_iron';
+        tier.repairIngredient = 'minecraft:glass';
+        tier.toughness = 0.0;
+        tier.knockbackResistance = 0.0;
+    });
 });
