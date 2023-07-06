59e1ca0e41e02ae868133efa8224f4f4212bad6b
Kazuhiko-Gushiken <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2023-07-05 19:45:20 -0700

    Add files via upload

---
+++ kubejs/server_scripts/recipetypes/aof/stonecutter.js
@@ -0,0 +1,7 @@
+    ////////////////////////
+    /// Made by Team AOF ///
+    ////////////////////////
+
+    ServerEvents.recipes(event => {
+        event.stonecutting('kubejs:invisible_ingot', 'minecraft:glass')
+    });
