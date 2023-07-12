4ae7a4d4db8fcd78612d40b2bd22a7ade48fc6f3
Kazuhiko-Gushiken <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2023-07-12 02:37:11 -0700

    Fixing a conflicting recipe.
    Sheet Metal Blocks and Iron Trapdoors had the same recipe. Changed the Sheet Metal Blocks to the stonecutter with the same output rate

---
+++ kubejs/server_scripts/recipetypes/architects_palette/stonecutter.js
@@ -0,0 +1,8 @@
+    ////////////////////////
+    /// Made by Team AOF ///
+    ////////////////////////
+
+    ServerEvents.recipes(event => {
+        event.stonecutting('4x architects_palette:sheet_metal_block', 'minecraft:iron_ingot')
+        .id("architects_palette:sheet_metal_block")
+    });
