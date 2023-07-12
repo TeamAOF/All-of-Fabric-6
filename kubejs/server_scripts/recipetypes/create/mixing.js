7b63ed423246abb374cf99d79c92293b50cb0083
AK9 <anthonkrisat@hotmail.com>
2023-07-12 19:55:13 +0200

    add mixing recipes for tree fertilizer & glue

---
+++ kubejs/server_scripts/recipetypes/create/mixing.js
@@ -0,0 +1,82 @@
+ServerEvents.recipes(event => {
+
+    // Tree Fertilizer
+    event.custom({
+        "type": "create:mixing",
+        "ingredients": [
+            {
+                "tag": "minecraft:flowers",
+            },
+            {
+                "tag": "minecraft:flowers",
+            },
+            {
+                "item": "botania:fertilizer",
+            },
+            {
+                "fluid": "minecraft:water",
+                "amount": 81000
+            }
+        ],
+        "results": [
+            {
+                "item": "create:tree_fertilizer",
+                "count": 4
+            },
+        ],
+    })
+
+    event.custom({
+        "type": "create:mixing",
+        "ingredients": [
+            {
+                "tag": "c:saplings",
+            },
+            {
+                "tag": "c:saplings",
+            },
+            {
+                "item": "botania:fertilizer",
+            },
+            {
+                "fluid": "minecraft:water",
+                "amount": 81000
+            }
+        ],
+        "results": [
+            {
+                "item": "create:tree_fertilizer",
+                "count": 3
+            },
+        ],
+    })
+
+    // Glue
+    event.custom({
+        "type": "create:mixing",
+        "ingredients": [
+            {
+                "item": "minecraft:bone_meal",
+            },
+            {
+                "item": "minecraft:bone_meal",
+            },
+            {
+                "item": "minecraft:bone_meal",
+            },
+            {
+                "item": "minecraft:bone_meal",
+            },
+            {
+                "fluid": "modern_industrialization:acrylic_glue",
+                "amount": 8100
+            }
+        ],
+        "results": [
+            {
+                "item": "kubejs:glue",
+                "count": 5
+            },
+        ],
+    })
+})
