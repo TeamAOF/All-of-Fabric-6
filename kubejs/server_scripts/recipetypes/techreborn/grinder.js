764ffe31c094b624254c959070bde159af941b2b
AK9 <anthonkrisat@hotmail.com>
2023-07-12 19:54:53 +0200

    add wood pulp recipe in grinder

---
+++ kubejs/server_scripts/recipetypes/techreborn/grinder.js
@@ -0,0 +1,25 @@
+////////////////////////
+/// Made by Team AOF ///
+////////////////////////
+
+
+ServerEvents.recipes(event => {
+
+  
+    event.custom({    
+      "type": "techreborn:grinder",
+      "power": 3,
+      "time": 100,
+      "ingredients": [
+        {
+          "tag": "minecraft:logs"
+        }
+      ],
+      "results": [
+        {
+          "item": "modern_industrialization:wood_pulp",
+          "count": 10
+        }
+      ]
+})
+    })
