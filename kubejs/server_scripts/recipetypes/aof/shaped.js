f318c5db226b5b3d3bc8e33e1820da329b622ee0
AK9 <anthonkrisat@hotmail.com>
2023-07-14 17:09:30 +0200

    fix quest book recipe

--- kubejs/server_scripts/recipetypes/aof/shaped.js
+++ kubejs/server_scripts/recipetypes/aof/shaped.js
@@ -1,49 +1,63 @@
-    ////////////////////////
-    /// Made by Team AOF ///
-    ////////////////////////
+////////////////////////
+/// Made by Team AOF ///
+////////////////////////
 
-    ServerEvents.recipes(event => {
+ServerEvents.recipes(event => {
 
-        const recipes = [
-    
-        //Invisisble Helmet
+    const recipes = [
+
+        // Invisisble Helmet
         {
             output: 'kubejs:invisible_helmet',
             pattern: ['III', 'I I', '   '],
             key: {
-            I: 'kubejs:invisible_ingot'
+                I: 'kubejs:invisible_ingot'
             },
+            id: 'aof:invisible_helmet'
         },
 
-        //Invisisble Chestplate
+        // Invisisble Chestplate
         {
             output: 'kubejs:invisible_chestplate',
             pattern: ['I I', 'III', 'III'],
             key: {
-            I: 'kubejs:invisible_ingot'
+                I: 'kubejs:invisible_ingot'
             },
+            id: 'aof:invisible_chestplate'
         },
 
-        //Invisible Leggings
+        // Invisible Leggings
         {
             output: 'kubejs:invisible_leggings',
             pattern: ['III', 'I I', 'I I'],
             key: {
-            I: 'kubejs:invisible_ingot'
+                I: 'kubejs:invisible_ingot'
             },
+            id: 'aof:invisible_leggings'
         },
 
-        //Invisible Boots
+        // Invisible Boots
         {
             output: 'kubejs:invisible_boots',
             pattern: ['   ', 'I I', 'I I'],
             key: {
-            I: 'kubejs:invisible_ingot'
+                I: 'kubejs:invisible_ingot'
             },
+            id: 'aof:invisible_boots'
         },
-        ];
-    
-        recipes.forEach((recipe) => {
-        event.shaped(recipe.output, recipe.pattern, recipe.key);
-        });
+
+        // Quest Book
+        {
+            output: 'ftbquests:book',
+            pattern: ['A'],
+            key: {
+                A: 'minecraft:book'
+            },
+            id: 'ftbquests:book'
+        },
+    ];
+
+    recipes.forEach((recipe) => {
+        event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
     });
+});
