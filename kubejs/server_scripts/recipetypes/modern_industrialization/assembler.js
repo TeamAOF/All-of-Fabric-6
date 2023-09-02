487e876ad419a9345ae1db0a227fee5a748771e7
Kazuhiko-Gushiken <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2023-09-02 11:29:25 -0700

    Changed assembler empty bag recipe to use acrylic glue

--- kubejs/server_scripts/recipetypes/modern_industrialization/assembler.js
+++ kubejs/server_scripts/recipetypes/modern_industrialization/assembler.js
@@ -6,19 +6,21 @@ ServerEvents.recipes(event => {
         "id": "aof:mi_assembler_empty_fertilizer_bag",
         "eu": 5,
         "duration": 200,
-        "item_inputs": [
+        "item_inputs": 
             {
                 "item": "minecraft:paper",
                 "amount": 7
             },
+        "fluid_inputs": 
             {
-                "item": "kubejs:glue",
-                "amount": 1
-            }
-        ],
-        "item_outputs": {
+                "fluid": "modern_industrialization:acrylic_glue",
+                "amount": 15
+            },
+        "item_outputs": [
+            {
             "item": "kubejs:empty_fertilizer_bag",
             "amount": 2
-        }
+            }
+        ]
     })
 })
